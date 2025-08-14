97d528a24d7e24d8b0a0b317d59f3b5f8c559884
anakarla99 <caballeroanakarla@gmail.com>
2025-08-14 20:27:17 +0200

    conection with sheet db

--- js-menu/menu.js
+++ js-menu/menu.js
@@ -1,11 +1,51 @@
-$(document).on('click', '.menu-link-js', function(e) {
-    e.preventDefault();
+$(document).ready(function() {
+    // Manejo de clicks en los enlaces del menú
+    $(document).on('click', '.menu-link-js', function(e) {
+        e.preventDefault();
+        var $self = $(this);
+        $('.menu-link-js').removeClass('color-primary');
+        $self.addClass('color-primary');
+        var target = $(this).attr('href');
+        $('.menu-tab-js:not(.menu-tab-static)').hide();
+        $(target).fadeIn();
+    });
 
-    var $self = $(this);
-    $('.menu-link-js').removeClass('color-primary');
-    $self.addClass('color-primary');
-    var target = $(this).attr('href');
+    // Carga datos de Google Sheets
+    fetch('https://sheetdb.io/api/v1/9c1zjw7nci6sj')
+        .then(response => response.json())
+        .then(data => {
+            // Agrupar items por categoría (menu)
+            const groupedData = {};
+            data.forEach(item => {
+                if (!groupedData[item.menu]) {
+                    groupedData[item.menu] = [];
+                }
+                groupedData[item.menu].push(item);
+            });
 
-    $('.menu-tab-js').hide();
-    $(target).fadeIn();
+            // Para cada categoría, agrega los items al <ul> correspondiente
+            for (const [category, items] of Object.entries(groupedData)) {
+                // Busca el <ul> dentro del div con id igual a la categoría
+                const menuTab = document.getElementById(category);
+                if (!menuTab) continue; // Si no existe, salta
+
+                const ul = menuTab.querySelector('ul.menu-list');
+                if (!ul) continue;
+
+                items.forEach(item => {
+                    const li = document.createElement('li');
+                    li.className = 'menu-list-item';
+                    li.innerHTML = `
+                    <h4 class="menu-list-item__title">${item.title}</h4>
+                    <div class="content">
+                        <span class="menu-list-item__desc">${item.desc || ''}</span>
+                        <span class="menu-list-item__dots"></span>
+                        <span class="menu-list-item__price">${item.price ? item.price + ' CUP' : ''}</span>
+                    </div>
+                `;
+                    ul.appendChild(li);
+                });
+            }
+        })
+        .catch(error => console.error("Error:", error));
 });
